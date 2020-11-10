5d8717eb39d5e183cc76f2389c19d78c4f356b54
Qiufang Shi <shiqiufang@hotmail.com>
2020-11-10 03:48:59 +0000

    更新icon

--- docker/images/demo/build.cmd
+++ docker/images/demo/build.cmd
@@ -1,1 +1,2 @@
-docker build %~dp0..\demo -t shiqiufang/n8n-demo:latest --no-cache
+#docker build %~dp0..\demo -t shiqiufang/n8n-demo:latest --no-cache
+docker build %~dp0..\demo -t shiqiufang/n8n-demo:latest
